8cbaa07c9a40fed4ec5e412f8daf70988a00ddf5
coderunner <bhagyeshbhagat@gmail.com>
2021-07-08 18:04:53 -0400

    Session 1

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>Coderunner Nest</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">Add Blog</a>
+            </div>
+        </nav>
+    );
+}
+ 
+export default Navbar;
