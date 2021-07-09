6fa540962882ca8ebd021a882c534c5a12db3b84
coderunner <bhagyeshbhagat@gmail.com>
2021-07-09 18:57:50 -0400

    Second Session

---
+++ src/BlogList.js
@@ -0,0 +1,18 @@
+const BlogList = (props) => {
+
+    const blogs = props.blogs;
+
+    return (
+        <div className="blog-list">
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{ blog.title }</h2>
+                    <p>{ blog.body }</p>
+                    <p>By: { blog.author }</p>
+                </div>
+            ))}
+        </div>
+    );
+}
+ 
+export default BlogList;
--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,16 @@
+import { useState } from 'react';
+import BlogList from './BlogList';
+
 const Home = () => {
+
+    const [blogs, setBlogs] = useState([{title: "Welcome to the Nest!", body: "lorem ipsum...", author: "Admin", id: 1},
+     {title: "Getting Started", body: "lorem ipsum...", author: "Morpheus", id: 2},
+     {title: "Tips for Netrunning", body: "lorem ipsum...", author: "Neo", id: 3},
+    ])
+
     return (
         <div className="home">
-            <h2>Homepage</h2>
+            <BlogList blogs={blogs} />
         </div>
     );
 }
